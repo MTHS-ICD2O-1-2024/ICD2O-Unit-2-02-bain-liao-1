4ab2f3a14ea8a7108a7210007ae32f93650b32c3
bain-liao-1 <liao.bain@stu.ocsb.ca>
2025-03-07 18:30:20 +0000

    Created area & perimeter program

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Bain Liao
+// Created on: Mar 2025
 // This file contains the JS functions for index.html
 
-/**
- * Displays an alert with the message "Hello, World!"
- */
-function myButtonClicked() {
-  alert("Hello, World!")
+function doMathClicked() {
+  document.getElementById("multiply-math").innerHTML = "<p>The area is: " + (5 * 3) + "cm² </p>"
+  document.getElementById("add-math").innerHTML = "<p>The perimeter is: " + (2 * (5 + 3)) + "cm </p>"
 }
